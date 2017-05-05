7849eab10bea700d7c5eab07302264f06675c1d5
Ryan Hutton <ryanchutton@gmail.com>
2017-05-05 10:06:56 -0700

    Changed the level end

--- Maze Runner/src/actor/MotionWithKeyBindings.java
+++ Maze Runner/src/actor/MotionWithKeyBindings.java
@@ -4,6 +4,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
  
