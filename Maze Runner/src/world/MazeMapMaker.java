f9b3064b5136996a9f93ac455d2929f00ab975fe
Ryan Hutton <ryanchutton@gmail.com>
2017-05-12 10:45:45 -0700

    Fixed map saving

--- Maze Runner/src/world/MazeMapMaker.java
+++ Maze Runner/src/world/MazeMapMaker.java
@@ -64,7 +64,7 @@ public class MazeMapMaker extends JFrame{
     }
     
     public void getMapList(){
-    	for(int i = 0; i < 99; i++){
+    	for(int i = 1; i < 99; i++){
     		File map = new File("data\\maps\\" + "./Level "+i+".map");
     		if(map.exists()){
     			System.out.println("Level "+i+" exists");
@@ -93,7 +93,7 @@ public class MazeMapMaker extends JFrame{
     
     public void saveMap(){
         try{
-        PrintWriter writer = new PrintWriter("data\\maps\\" + "Level "+level+".map", "UTF-8");
+        PrintWriter writer = new PrintWriter("data\\maps\\" + "Level "+ (level + 1) +".map", "UTF-8");
         for(int y = 0; y < columns; y++){
             for(int x = 0; x < rows; x++){
                 writer.print(map[x][y]);
