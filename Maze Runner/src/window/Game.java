628f19226642831641001d23599ef8e84ee40de8
Ryan Hutton <ryanchutton@gmail.com>
2017-05-17 09:38:20 -0700

    Started Rebuilding Engine

---
+++ Maze Runner/src/window/Game.java
@@ -0,0 +1,51 @@
+package window;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Game extends JPanel{
+	
+	public static Game game = new Game();
+	
+	int x = 0;
+	int y = 0;
+
+	private void moveBall() {
+		x = x + 1;
+		y = y + 1;
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setColor(Color.CYAN);
+		g2d.fillRect(x, y, 25, 25);
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Maze Runner");
+		frame.add(game);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		while (true) {
+			game.moveBall();
+			game.repaint();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
