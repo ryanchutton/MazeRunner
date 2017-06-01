365c9d833053e9483e612c1c5842f0f72c0f12d3
ryanchutton <ryanchutton@gmail.com>
2017-06-01 12:20:52 -0700

    Tidied up greeting message

--- Maze Runner/src/window/Game.java
+++ Maze Runner/src/window/Game.java
@@ -9,60 +9,63 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class Game extends JPanel {
 
-	public static Game game;
-	public static int frameWidth = 500, frameHeight = 500;
-	public static actor.Player p;
-	public static actor.KeyManager key;
-	public static world.Maze m;
-	private static String str;
-	public static JFrame frame;
+    public static Game game;
+    public static int frameWidth = 500, frameHeight = 500;
+    public static actor.Player p;
+    public static actor.KeyManager key;
+    public static world.Maze m;
+    private static String str;
+    public static JFrame frame;
 
-	public void paint(Graphics g) {
-		super.paint(g);
-		m.paint(g);
-		p.paint(g);
-	}
+    public void paint(Graphics g) {
+        super.paint(g);
+        m.paint(g);
+        p.paint(g);
+    }
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		create();
-		
-		while (true) {
-			game.repaint();
-			key.update();
-			Thread.sleep(125);
-		}
-	}
+    public static void main(String[] args) throws IOException, InterruptedException {
+        create();
+        
+        while (true) {
+            game.repaint();
+            key.update();
+            Thread.sleep(125);
+        }
+    }
 
-	public static void loadMap() throws IOException {
-		str = JOptionPane.showInputDialog(null, "WELCOME TO MAZE RUNNER \n\n\n\n\n\n\n "
-				+ "How To Play: \n\n\n 1) Collect the yellow blocks to unlock the "
-				+ "green doors \n\n\n 2) Push red and blue blocks \n\n\n 3)Get to "
-				+ "the cyan blocks to finish the level! \n\n\n\n\n Please enter level number. Type \"quit\" to quit.");
-		if (str.length() < 1) {
-			loadMap();
-		}
-		else if(str.toLowerCase().equals("quit")){		
-			System.exit(0);
-		}
-		m = new world.Maze("data\\maps\\" + "Level " + str + ".map"); // "data\\maps\\"
-																		// +
-																		// "Level
-																		// "+"1"+".map"
-	}
+    public static void loadMap() throws IOException {
+        str = JOptionPane.showInputDialog(null, "WELCOME TO MAZE RUNNER \n"
+                + "How To Play: \n  1) Collect the yellow blocks to unlock the "
+                + "green doors \n  2) Push red and blue blocks \n  3) Get to "
+                + "the cyan blocks to finish the level! \n\n Please enter level number. Type \"quit\" to quit.\n");
+        if (str == null) {
+            System.exit(0);
+        }
+        if (str.length() < 1) {
+            loadMap();
+        }
+        else if(str.toLowerCase().equals("quit")){      
+            System.exit(0);
+        }
+        m = new world.Maze("data\\maps\\" + "Level " + str + ".map"); // "data\\maps\\"
+                                                                        // +
+                                                                        // "Level
+                                                                        // "+"1"+".map"
+    }
 
-	public static void create() throws IOException {
+    public static void create() throws IOException {
 
-		game = new Game();
-		p = new actor.Player();
-		key = new actor.KeyManager();
-		loadMap();
-		frame = new JFrame("Maze Runner");
-		frame.setSize(750, 750);
-		frame.add(game);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game = new Game();
+        p = new actor.Player();
+        key = new actor.KeyManager();
+        loadMap();
+        frame = new JFrame("Maze Runner");
+        frame.setSize(750, 750);
+        frame.add(game);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frame.addKeyListener(key);
-	}
+        frame.addKeyListener(key);
+    }
 
 }
